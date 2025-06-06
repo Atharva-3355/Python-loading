52a4a91a8d1a3ab11165bf147f88be2f14d11579
Your Name <you@example.com>
2025-06-06 21:35:43 +0530

    commit 3

--- chp1_variables.py
+++ chp1_variables.py
@@ -1,11 +1,11 @@
-roll_no=3 #integer
-student_name = "Atharva Agre" #string
-student_proness_ratio = 99.999 #float
-students_list = ["Atharva" , "ATHARVA" , "AthArvA"] #list
+roll_no = 3  # integer
+student_name = "Atharva Agre"  # string
+student_proness_ratio = 99.999  # float
+students_list = ["Atharva", "ATHARVA", "AthArvA"]  # list
 
-item1_oflist = students_list[0] #list item
+item1_oflist = students_list[0]  # list item
 
-#lets print em all !!!
-print(roll_no)
-print(roll_no,student_name,student_proness_ratio,students_list,item1_oflist)
+# lets print em all !!!
 
+print(roll_no, student_name,
+      student_proness_ratio, students_list, item1_oflist)
--- patanahijiyekyahai.py
+++ patanahijiyekyahai.py
@@ -1,50 +1,50 @@
-# str = "hello"www
-# tup = ("Hello", 5 ,"a")
-# list = [1 , 2 ,3]
-# list[0] = 11
-sum = 0
-i = 0
-roww = int(input("Enter size:"))
-j = 0
+# # str = "hello"www
+# # tup = ("Hello", 5 ,"a")
+# # list = [1 , 2 ,3]
+# # list[0] = 11
+# sum = 0   
+# i = 0
+# roww = int(input("Enter size:"))
+# j = 0
 
-r1 = roww
-k = r1-1
-star = k
-for j in range(0, roww):
+# r1 = roww
+# k = r1-1
+# star = k
+# for j in range(0, roww):
 
-    i = 1
+#     i = 1
 
-    for i in range(1, r1):
-        print(" ", end="")
+#     for i in range(1, r1):
+#         print(" ", end="")
 
-    r1 = r1-1
+#     r1 = r1-1
 
-    while (star < roww):
-        print("*", end="")
-        star += 1
-    print("")
-    k -= 2
-    star = k
+#     while (star < roww):
+#         print("*", end="")
+#         star += 1
+#     print("")
+#     k -= 2
+#     star = k
 
-roww -= 1
-r1 = roww
-k = r1-1
-star = k
+# roww -= 1
+# r1 = roww
+# k = r1-1
+# star = k
 
-for j in range(0, roww):
+# for j in range(0, roww):
 
-    i =1
+#     i =1
 
-    while (star < roww):
-        print(" ", end="")
-        star += 1
+#     while (star < roww):
+#         print(" ", end="")
+#         star += 1
 
-    for i in range(1, r1*2):
-        print("*", end="")
+#     for i in range(1, r1*2):
+#         print("*", end="")
 
-    print("")
+#     print("")
 
-    r1 = r1-1
+#     r1 = r1-1
 
-    k -= 1
-    star = k
+#     k -= 1
+#     star = k
